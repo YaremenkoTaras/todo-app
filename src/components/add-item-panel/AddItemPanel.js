7ca51fff0c10ce94bef2b33867c8056e968fd913
Taras Yaremenko <tyaremenko@atlassian.com>
2019-12-22 20:16:14 +0200

    RRL-44-45

--- src/components/add-item-panel/AddItemPanel.js
+++ src/components/add-item-panel/AddItemPanel.js
@@ -1,16 +1,36 @@
 import React from "react";
 
-const AddItemPanel = ({onItemAdded}) => {
-    return (
-        <div className="d-flex">
-            <input placeholder="Add new todo's note"
-                   onSubmit={() => console.log('clicked add')}/>
-            <button type="button" className="btn btn-info"
-                    onClick={() => onItemAdded("new item")}>
-                Add
-            </button>
-        </div>
-    );
-};
+export default class AddItemPanel extends React.Component {
+    state = {
+        label: ''
+    };
 
-export default AddItemPanel;
+    render() {
+        return (
+            <form className="add-item-panel d-flex"
+                  onSubmit={this.onSubmit}>
+                <input type="text"
+                       placeholder="What need's to be done?"
+                       onChange={this.onLabelChange}
+                       value={this.state.label}/>
+                <button className="btn btn-info">
+                    Add
+                </button>
+            </form>
+        );
+    }
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        })
+    };
+};
