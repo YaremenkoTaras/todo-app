b6716839474f7d6b3e3e3f25fe681cba1be467c0
Taras Yaremenko <tyaremenko@atlassian.com>
2019-12-22 16:12:24 +0200

    RRL-36

--- src/components/todo-list-item/TodoListItem.js
+++ src/components/todo-list-item/TodoListItem.js
@@ -3,18 +3,29 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+    state = {
+        done: false
+    };
+
     onLabelClick = () => {
-        console.log(`Clicked on text ${this.props.label}`);
-    }
+        this.setState({
+            done: !this.state.done
+        });
+    };
 
     render() {
-        let {label, important} = this.props;
+        const {label, important = false} = this.props;
+        const {done} = this.state;
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
+        let classNames = "todo-list-item";
+        if (done) {
+            classNames += " done";
+        }
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
                 <span className="todo-list-item-label" style={style}
                       onClick={this.onLabelClick}>
                     {label}
