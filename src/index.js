14a0207fae96c9414117d283cd371d3e24b789f3
Taras Yaremenko <tyaremenko@atlassian.com>
2019-12-22 13:43:41 +0200

    RRL-26

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDom from 'react-dom';
 
 const TodoList = () => {
+    let items = ['Learn react 1', 'Build Awesome App 2'];
     return <ul>
-        <li>Learn react</li>
-        <li>Build Awesome App</li>
+        <li>{items[0]}</li>
+        <li>{items[1]}</li>
     </ul>;
 };
 
 const SearchPanel = () => {
-    return <input placeholder="search"/>;
+    const searchText = 'Type here to search...';
+    return <input placeholder={searchText}/>;
 };
 
 const AppHeader = () => {
